a1ffb171375b05e050c962b08440cf7a27fa7591
Venci <ventsi@lightningstep.com>
2024-08-04 22:42:23 +0300

    remove coments

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -49,24 +49,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-
-//    Route::get('/roles/index/', [RoleController::class, 'index'])->name('roles.index');
-//    Route::post('/roles/store', [RoleController::class, 'update'])->name('roles.store');
-//    Route::get('/roles/create/', [RoleController::class, 'edit'])->name('roles.edit');
-//    Route::get('/roles/edit/{id}', [RoleController::class, 'edit'])->name('roles.edit');
-//    Route::get('/roles/show/{id}', [RoleController::class, 'show'])->name('roles.show');
-//    Route::patch('/roles/update/{id}', [RoleController::class, 'update'])->name('roles.update');
-//    Route::get('/roles/destroy/{id}', [RoleController::class, 'destroy'])->name('roles.destroy');
-//
-//
-//    Route::get('/users/index/', [UserController::class, 'index'])->name('users.index');
-//    Route::get('/users/create/', [UserController::class, 'create'])->name('users.create');
-//    Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
-//    Route::get('/users/show/{id}', [UserController::class, 'show'])->name('users.show');
-//    Route::patch('/users/update/{id}', [UserController::class, 'update'])->name('users.update');
-//    Route::post('/users/store', [UserController::class, 'update'])->name('users.store');
-//    Route::get('/users/destroy/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-
     Route::resource('roles', RoleController::class);
     Route::resource('users', UserController::class);
 
